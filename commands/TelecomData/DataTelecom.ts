f4478a3347f2de2e78034dddfc39f5f590e0606e
Bünyamin Can Dağ <bunyamincan.dag@outlook.com>
2023-03-15 23:51:09 +0300

    done refactore

--- commands/TelecomData/DataTelecom.ts
+++ commands/TelecomData/DataTelecom.ts
@@ -13,18 +13,8 @@ export default class DataTelecom extends BaseCommand {
   public static description = 'Get telecom providers data.'
 
   public static settings = {
-    /**
-     * Set the following value to true, if you want to load the application
-     * before running the command. Don't forget to call `node ace generate:manifest`
-     * afterwards.
-     */
     loadApp: true,
 
-    /**
-     * Set the following value to true, if you want this command to keep running until
-     * you manually decide to exit the process. Don't forget to call
-     * `node ace generate:manifest` afterwards.
-     */
     stayAlive: false,
   }
 
